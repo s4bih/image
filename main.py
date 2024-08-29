35beb1a84b8cc5d3dc0ad7a47ddc6592e648ca1e
s4bih <msabihamin@gmail.com>
2024-08-29 21:56:59 +0800

    image 5%

--- main.py
+++ main.py
@@ -19,7 +19,27 @@ def show_image():
 def applay(image):
     gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     return gray
+def get_path():
+    root = Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+    image_path = filedialog.askopenfilename(title="Select image", filetypes=(("image", "*.jpg"), ("all files", "*.*")))
+
+    if image_path == "":
+        print=("No image selected")
+        return
+
+    image=cv2.imread(image_path)
+    cv2.imshow("image",image)
+
+    gray_image=applay(image)
+    cv2.imshow("gray_image",gray_image)
+
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    show_image()
+
+    get_path()

d1d8d48f30dfd032aa6bbb0311ecb5eddf71bfb0
s4bih <msabihamin@gmail.com>
2024-08-30 21:38:20 +0800

    image 5%

--- poitong potong.py
+++ poitong potong.py
@@ -3,6 +3,7 @@ import cv2
 image = "th.jpeg"
 
 image_path = cv2.imread(image)
+'''
 def crop_image(image,x,y,w,h):
     # cropping image
     crop_img = image[y:y+h, x:x+w]
@@ -22,8 +23,24 @@ rezized_image = resize_image(image_path, 50)
 cv2.imshow('Resized Image', rezized_image)
 
 
+
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+'''
+def rotate_image(image, angle):
+    center = (image.shape[1] // 2, image.shape[0] // 2)
+    rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated_image = cv2.warpAffine(image, rotation_matrix, (image.shape[1], image.shape[0]))
+    return rotated_image
+
+rotated_image = rotate_image(image_path, 180)
+cv2.imshow ('Original Image', image_path)
+cv2.imshow('Rotated Image', rotated_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
 
 
